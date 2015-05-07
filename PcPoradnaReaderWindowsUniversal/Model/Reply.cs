562fa2216ea7a1f7aca60f15dff14752be214eb2
Martin Pernica <martin.developer@hotmail.com>
2015-05-07 10:04:01 +0200

    Created Reply class

---
+++ PcPoradnaReaderWindowsUniversal/Model/Reply.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PcPoradnaReaderWindowsUniversal.Model
+{
+    class Reply
+    {
+        public int Id { get; set; }
+
+        public Reply Parent { get; set; }
+
+        public Uri WebUrl { get; set; }
+
+        public string Title { get; set; }
+
+        public User Author { get; set; }
+
+        public DateTime CreatedOn { get; set; }
+
+        public bool IsDeleted { get; set; }
+
+        public string Text { get; set; }
+    }
+}

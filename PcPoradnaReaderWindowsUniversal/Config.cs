a7e844075f6b6a001de7a725c1221c8706ecec2e
Martin Pernica <martin.developer@hotmail.com>
2015-05-07 09:42:30 +0200

    Added Config class

---
+++ PcPoradnaReaderWindowsUniversal/Config.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PcPoradnaReaderWindowsUniversal
+{
+    public enum EndpointType
+    {
+        JSON = 0
+    }
+
+    class Config
+    {
+        public EndpointType ApiTyp { get; } = EndpointType.JSON;
+
+        public string LatestQuestionsEndpoint { get; } = "http://pc.poradna.net/q/index.json";
+    }
+}
